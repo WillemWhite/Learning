2b5755ecf01d10daef9ac64fed8cb3b6ff8276b9
Willem Black <80010724+WillemWhite@users.noreply.github.com>
2021-03-10 02:30:52 +0300

    Added task_2

---
+++ 2nd_semester/task_2/main.cpp
@@ -0,0 +1,45 @@
+﻿/*
+ Пользуясь рекуррентными формулами, вычислить значения многочлена степеней 
+ от 0 до N при заданном значении аргумента X, принадлежащего области D. 
+ Вычисление значений оформить в виде рекурсивной функции.
+ U[n+1](x) = (1/(n+1)) * ((2*n+1-x)*U[n](x) - n*U[n-1](x))
+ U[0](x) = 0
+ U[1](x) = -x+1
+ Область определения аргумента (D): x > 0
+ Написать функцию тестирующую результат вычисления при помощи assert.
+ Определить обе функции в отдельной единице трансляции. 
+ Подключить объявления функций в main.cpp
+ при помощи заголовочного файла.
+ */
+#include <iostream>
+#include "pol.h"
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Please enter the degree and argument of the polynomial\n";
+
+    std::cout << "Degree: ";
+
+    int mainDegree;
+    std::cin >> mainDegree;
+    std::cin.clear();
+    std::cout << "\n";
+
+    std::cout << "Argument: ";
+
+    float mainArgument;
+
+    std::cin >> mainArgument;
+    std::cin.clear();
+    std::cout << "\n";
+
+    inpConditions(mainDegree, mainArgument);
+    testPolComputation();
+
+    std::cout << "Your polynomial value: "
+              << polComputation(mainDegree, mainArgument) << "\n";
+
+    std::cin.get();
+    std::cin.get();
+    return 0;
+}
---
+++ 2nd_semester/task_2/polynomial.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <cassert>
+
+float polComputation(int, float);
+
+void inpConditions(int deg, float arg)
+{
+    assert(arg > 0 && "Wrong input: out of range argument... (argument > 0)");
+    assert(deg >= 0 && "Wrong input: degree must be >= 0");
+}
+void testPolComputation()
+{
+    char mes[16] = "Wrong tests...";
+
+    assert(polComputation(2, 1) == 0 && mes);
+    assert(polComputation(3, 5) == static_cast<float>(8.0 / 3.0) && mes);
+    assert(polComputation(3, 2) == static_cast<float>(-0.5 + (2.0 / 3.0)) && mes);
+}
+
+float polComputation(int deg, float arg)
+{
+    switch (deg)
+    {
+    case 0:
+        return 0;
+        break;
+
+    case 1:
+        return (-arg + 1);
+        break;
+
+    default:
+        return (((1.0 / deg) * ((2 * deg - 1 - arg) * polComputation(deg - 1, arg) -
+                                (deg - 1) * polComputation(deg - 2, arg))));
+    }
+}
